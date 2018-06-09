4c8024eb4085c26a3e1aa24e31a6f1e81f90c8d8
abbey <abbey@cattestcentre.com>
2018-06-09 18:24:07 +0100

    Game now lets you fill the grid, exits when full.

--- src/sudokugame/SudokuMain.java
+++ src/sudokugame/SudokuMain.java
@@ -1,4 +1,5 @@
 package sudokugame;
+import java.util.Scanner;
 
 public class SudokuMain {
 	static void drawGame(int[][] gamegrid) {
@@ -24,7 +25,7 @@ public class SudokuMain {
 	}
 	
 	public static void main(String[] args) {
-		int[][] grid = new int[][] {
+		int[][] startgrid = new int[][] {
 			{0,0,0,2,6,0,7,0,1},
 			{6,8,0,0,7,0,0,9,0},
 			{1,9,0,0,0,4,5,0,0},
@@ -35,11 +36,59 @@ public class SudokuMain {
 			{0,4,0,0,5,0,0,3,6},
 			{7,0,3,0,1,8,0,0,0}
 		};
+		int[][] grid = new int[9][9];
+		for (int rows = 0; rows < 9; rows++) {
+			for (int cols = 0; cols < 9; cols++) {
+				grid[rows][cols] = startgrid[rows][cols];
+			}
+		}
 		
+		System.out.println("Welcome to Sudoku!");
+		System.out.println("Here is the starting grid:");
+		Scanner scan = new Scanner(System.in);
+		drawGame(startgrid);
 		
+		boolean cont = true;
+		do {
+			
+			System.out.println("Choose a row:");
+			int row = scan.nextInt() - 1;
+			
+			System.out.println("Choose a column:");
+			int column = scan.nextInt() - 1;
+			
+			
+			System.out.println("Choose a number:");
+			int value = scan.nextInt();
+			
+			
+			
+			if (startgrid[row][column] == 0) {
+				grid[row][column] = value;
+			} else {
+				System.out.println("Invalid entry.  Attempted overwrite of starting position.");
+			}
+			
+			drawGame(grid);
+			
+			/*
+			 * Checking to see if there are any zeros left in play - if not, then end the game.
+			 */
+			
+			boolean iszero = true;
+			for (int[] rows : grid) {
+				for (int val : rows) {
+					if (val == 0) {
+						iszero = true;
+					}
+				}
+			}
+			if (iszero != true) {
+				scan.close();
+				cont = false;
+			}
+		} while (cont == true);
 		
-		
-		drawGame(grid);
 	}
 	
 	
